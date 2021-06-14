bc0763349a261e1e65f99867263cce28836f261e
vebalba <vebalba@student.apc.edu.ph>
2021-06-14 13:38:12 +0800

    updated News_model.php

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -9,11 +9,11 @@ class News_model extends CI_Model {
         {
         if ($slug === FALSE)
         {
-                $query = $this->db->get('news');
+                $query = $this->db->get('balba_vina_news');
                 return $query->result_array();
         }
 
-        $query = $this->db->get_where('news', array('slug' => $slug));
+        $query = $this->db->get_where('balba_vina_news', array('slug' => $slug));
         return $query->row_array();
         }
 }
